1b28c183069b9948d0955e9cbbd8721ecb728b9f
Pippi <@>
2023-05-22 03:53:07 +0200

    ⠀⠀⠀⠀

---
+++ cmd/a2s/a2s.go
@@ -0,0 +1,80 @@
+// Copyright 2012 - 2018 The txt2svg Contributors
+// All rights reserved.
+
+// The a2s tool generates SVG output given an ASCII diagram input.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/pippi101/txt2svg"
+)
+
+const logo = ` .-------------------------.
+ |                         |
+ | .---.-. .-----. .-----. |
+ | | .-. | +-->  | |  <--+ |
+ | | '-' | |  <--+ +-->  | |
+ | '---'-' '-----' '-----' |
+ |  ascii     2      svg   |
+ |                         |
+ '-------------------------'
+
+https://github.com/pippi101
+
+[1,0]: {"fill":"#88d","a2s:delref":1}
+`
+
+func mainImpl() error {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "%s\n", logo)
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	in := flag.String("i", "-", "Path to input text file. If set to \"-\" (hyphen), stdin is used.")
+	out := flag.String("o", "-", "Path to output SVG file. If set to \"-\" (hyphen), stdout is used.")
+	noBlur := flag.Bool("b", false, "Disable drop-shadow blur.")
+	font := flag.String("f", "Consolas,Monaco,Anonymous Pro,Anonymous,Bitstream Sans Mono,monospace", "Font family to use.")
+	scaleX := flag.Int("x", 9, "X grid scale in pixels.")
+	scaleY := flag.Int("y", 16, "Y grid scale in pixels.")
+	tabWidth := flag.Int("t", 8, "Tab width.")
+	doLogo := flag.Bool("L", false, "Generate SVG of the a2s logo.")
+	flag.Parse()
+
+	var input []byte
+	var err error
+	if *doLogo {
+		input = []byte(logo)
+	} else {
+		if *in == "-" {
+			input, err = ioutil.ReadAll(os.Stdin)
+		} else {
+			input, err = ioutil.ReadFile(*in)
+		}
+	}
+	if err != nil {
+		return err
+	}
+
+	canvas, err := txt2svg.NewCanvas(input, *tabWidth, *noBlur)
+	if err != nil {
+		return err
+	}
+	svg := txt2svg.CanvasToSVG(canvas, *noBlur, *font, *scaleX, *scaleY)
+	if *out == "-" {
+		_, err := os.Stdout.Write(svg)
+		return err
+	}
+	return ioutil.WriteFile(*out, svg, 0666)
+}
+
+func main() {
+	if err := mainImpl(); err != nil {
+		fmt.Fprintf(os.Stderr, "a2s: %s\n", err)
+		os.Exit(1)
+	}
+}
---
+++ svg_test.go
@@ -0,0 +1,121 @@
+// Copyright 2012 - 2018 The txt2svg Contributors
+// All rights reserved.
+
+package txt2svg
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/maruel/ut"
+)
+
+func TestCanvasToSVG(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		input  []string
+		length int
+	}{
+		// 0 Box with dashed corners and text
+		{
+			[]string{
+				"+--.",
+				"|Hi:",
+				"+--+",
+			},
+			1677,
+		},
+
+		// 1 Box with non-existent ref
+		{
+			[]string{
+				".-----.",
+				"|[a]  |",
+				"'-----'",
+			},
+			1763,
+		},
+
+		// 2 Box with ref, change background color of container with #RRGGBB
+		{
+			[]string{
+				".-----.",
+				"|[a]  |",
+				"'-----'",
+				"",
+				"[a]: {\"fill\":\"#000000\"}",
+			},
+			1858,
+		},
+
+		// 3 Box with ref && fill, change label
+		{
+			[]string{
+				".-----.",
+				"|[a]  |",
+				"'-----'",
+				"",
+				"[a]: {\"fill\":\"#000000\",\"a2s:label\":\"abcdefg\"}",
+			},
+			1826,
+		},
+
+		// 4 Box with ref && fill && label, remove ref
+		{
+			[]string{
+				".-----.",
+				"|[a]  |",
+				"'-----'",
+				"",
+				"[a]: {\"fill\":\"#000000\",\"a2s:label\":\"abcd\",\"a2s:delref\":1}",
+			},
+			1764,
+		},
+
+		// 5 Ticks and dots in lines.
+		{
+			[]string{
+				" ------x----->",
+				"",
+				" <-----o------",
+			},
+			1968,
+		},
+
+		// 6 Just text
+		{
+			[]string{
+				" foo",
+			},
+			1476,
+		},
+
+		// 7 Just text with a deleting reference
+		{
+			[]string{
+				" foo",
+				"[1,0]: {\"a2s:delref\":1,\"a2s:label\":\"foo\"}",
+			},
+			1477,
+		},
+
+		// 8 Just text with a link
+		{
+			[]string{
+				" foo",
+				"[1,0]: {\"a2s:delref\":1, \"a2s:link\":\"https://github.com/pippi101/txt2svg\"}",
+			},
+			1521,
+		},
+	}
+	for i, line := range data {
+		canvas, err := NewCanvas([]byte(strings.Join(line.input, "\n")), 9, false)
+		if err != nil {
+			t.Fatalf("Error creating canvas: %s", err)
+		}
+		actual := string(CanvasToSVG(canvas, false, "", 9, 16))
+		// TODO(dhobsd): Use golden file? Worth postponing once output is actually
+		// nice.
+		ut.AssertEqualIndex(t, i, line.length, len(actual))
+	}
+}
